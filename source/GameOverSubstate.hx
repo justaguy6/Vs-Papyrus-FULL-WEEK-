9c3cac1a4399143591a0f8298025093c9e887ab6
JoseXD <100051827+JoseXD2@users.noreply.github.com>
2022-09-12 15:04:31 -0300

    Update GameOverSubstate.hx

--- source/GameOverSubstate.hx
+++ source/GameOverSubstate.hx
@@ -118,6 +118,10 @@ class GameOverSubstate extends MusicBeatSubstate
 		camFollowPos = new FlxObject(0, 0, 1, 1);
 		camFollowPos.setPosition(FlxG.camera.scroll.x + (FlxG.camera.width / 2), FlxG.camera.scroll.y + (FlxG.camera.height / 2));
 		add(camFollowPos);
+		
+		#if android
+		addVirtualPad(NONE, A_B);
+		#end
 
 		if (PlayState.deathCounter == 5 && !ClientPrefs.disableMecha){
 			blockRetry = true;

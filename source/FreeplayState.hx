00d14cda9125c88dc70df517e0d585530c4d87ce
JoseXD <100051827+JoseXD2@users.noreply.github.com>
2022-09-12 15:03:15 -0300

    Shit

--- source/FreeplayState.hx
+++ source/FreeplayState.hx
@@ -280,6 +280,9 @@ class FreeplayState extends MusicBeatState
 		text.setFormat(Paths.font("vcr.ttf"), size, FlxColor.WHITE, RIGHT);
 		text.scrollFactor.set();
 		add(text);
+		#if android
+		addVirtualPad(LEFT_FULL, A_B);
+		#end
 		super.create();
 	}
 

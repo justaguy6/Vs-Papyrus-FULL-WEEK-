a5efa230cff380259deef768749a2510c4bc3ccc
JoseXD <100051827+JoseXD2@users.noreply.github.com>
2022-09-12 16:54:49 -0300

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -107,7 +107,7 @@ class Main extends Sprite
 	// Code was entirely made by sqirra-rng for their fnf engine named "Izzy Engine", big props to them!!!
 	// very cool person for real they don't get enough credit for their work
 	
-	#if CRASH_HANDLER
+	#if (CRASH_HANDLER && desktop)
 	function onCrash(e:UncaughtErrorEvent):Void
 	{
 		var errMsg:String = "";

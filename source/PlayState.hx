2a420c311aa83b9926a04ed398d0b45d55f1c19e
JoseXD <100051827+JoseXD2@users.noreply.github.com>
2022-09-20 15:26:32 -0300

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1699,9 +1699,7 @@ addAndroidControls();
 					if(daSong == 'roses') FlxG.sound.play(Paths.sound('ANGRY'));
 					schoolIntro(doof);
 				case 'bad-to-the-bone':
-					var video:VideoHandler = new VideoHandler();
-					video.playVideo(Paths.video('intro'));
-					video.finishCallback = function() 
+					
 					{
 						papyintro(doof);
 					}

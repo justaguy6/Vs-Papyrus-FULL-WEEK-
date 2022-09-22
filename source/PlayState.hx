285bf7252fadeb7787710ff5a1d6b6273476b01c
JoseXD <100051827+JoseXD2@users.noreply.github.com>
2022-09-22 17:23:53 -0300

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1715,9 +1715,7 @@ addAndroidControls();
 					if(daSong == 'roses') FlxG.sound.play(Paths.sound('ANGRY'));
 					schoolIntro(doof);
 				case 'bad-to-the-bone':
-					var video:VideoHandler = new VideoHandler();
-					video.playVideo(Paths.video('intro'));
-					video.finishCallback = function() 
+					LoadingState.loadAndSwitchState(new VideoState(Paths.video('intro'), new PlayState()));
 					
 					{
 						papyintro(doof);
@@ -5010,7 +5008,7 @@ addAndroidControls();
 						{
 							//if(!ClientPrefs.getGameplaySetting('practice', false) && !ClientPrefs.getGameplaySetting('botplay', false))
 								//FreeplayState.songUnlock[2] = true;
-							
+							LoadingState.loadAndSwitchState(new VideoState(Paths.video('final'), new PlayState()));
 							
 							 {
 							
